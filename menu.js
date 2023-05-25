d4b2d5094234a9509ad44708ec51a02e02ee52b1
annauzun <terziann@mail.ru>
2023-05-25 15:55:19 +0300

    mobile version

--- menu.js
+++ menu.js
@@ -2,14 +2,17 @@ let isOpen = false
 
 function openMenu() {
     const container = document.getElementById("mobile-navigation")
+    const button = document.getElementById("mobile-button")
 
     if (isOpen) {
         container.style.display = "none"
+        button.innerHTML = '<img src="images/menu_icon.png" id="menu-icon">'
+
         isOpen = false
     } else {
         container.style.display = "flex"
+        button.innerHTML = '<img src="images/close_menu_icon.png" id="menu-icon">'
+
         isOpen = true
-    }
-    
-    
+    }    
 }
